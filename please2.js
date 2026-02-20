4b87027382748efdc2e634d3fc68bf8dd54a5d18
Brittany <brittanyluvera@gmail.com>
2026-02-20 17:36:33 -0500

    hearts

--- please2.js
+++
@@ -1,6 +0,0 @@
-function clickHooray() {
-  alert("Hooray! now you have to spend the snow day with brittany!🤗🤗");
-}
-
-let specialButton = document.querySelector("#special-button");
-specialButton.addEventListener("click", clickHooray);
